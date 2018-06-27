01c03082649137379a29f256cfbcbefa664ba8a8
coniekp <coniekp@icloud.com>
2018-06-27 11:28:13 -0700

    Build GroceryList component

--- app.js
+++ app.js
@@ -1,1 +1,58 @@
-// TODO
+var GroceryList = () => (
+  <div>
+    <h1>GroceryList</h1>
+    <ItemsList items={['Cofee', 'Tea', 'Soda', 'Wine']}/>
+  </div>
+);
+
+var ItemsList = (props) => {
+
+    var rows = [];
+    props.items.forEach((item, index) => {
+      rows.push(
+        <Item key={index} item={item} />
+      );
+    });
+
+    return (
+      <ul>{rows}</ul>
+    );
+
+};
+
+// STATELESS ITEM COMPONENT BEFORE REFACTOR TO CLASS COMPONENT
+//
+// var Item = (props) => {
+//   var onListItemClick = (event) => {
+//     console.log('Clicked!');
+//   };
+  
+//   return (
+//     <li onClick={onListItemClick}>{props.item}</li>
+//     );
+// };
+
+class Item extends React.Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+      bought: false
+    }
+  }
+  
+  onListItemClick () {
+    this.setState({
+      bought: !this.state.bought
+    });
+  }
+  
+  render () {
+    var style = {textDecoration: this.state.bought ? 'line-through' : 'none'};
+    
+    return (
+     <li key={this.props.index} style={style} onClick={this.onListItemClick.bind(this)}>{this.props.item}</li>
+    )
+  }
+}
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
